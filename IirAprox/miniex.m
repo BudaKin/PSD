f7184a2cbe93944e8d8d41545c085e066843096e
BudaKin <igorcrashtom@hotmail.com>
2025-07-03 11:12:33 -0300

    Aula 03/07/25

---
+++ IirAprox/miniex.m
@@ -0,0 +1,9 @@
+% raízes do polinomio de 8a ordem
+roots([0.5088^2 0 0 0 0 0 0 0 1]) 
+% Resultam em 8 raízes
+% Pegar metade (a que possui parte real negativa)
+poly([-1.0939 + 0.4531i
+  -1.0939 - 0.4531i
+  -0.4531 + 1.0939i
+  -0.4531 - 1.0939i
+   ])
