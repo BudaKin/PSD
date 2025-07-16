8dfa312cdf484bec106ee2b206325b5c89c10658
BudaKin <igorcrashtom@hotmail.com>
2025-07-16 15:02:13 -0300

    Finalização do código utilizado na elaboração do relatório

--- IirAprox/ex2.m
+++ IirAprox/ex2.m
@@ -1,6 +1,5 @@
-clear all; close all; clc;
+close all;
 
-load('matlab.mat')
 
 %% Sinal
 freq1 = 770;
@@ -20,24 +19,67 @@ S = fftshift(S);
 freq = Fs*(-(L/2):(L/2)-1)/L;
 
 %% Filtros IIR importados do filterDesigner 
-% figure,freqz(num_low1,den_low1);
-% figure,freqz(num_low2,den_low2);
-% figure,freqz(num_low3,den_low3);
+load('filtiir.mat')
 
-s_f_low1 = filter(num_low1,den_low1,s);
-S_F_low1 = fft(s_f_low1);
-S_F_low1 = abs(2*S_F_low1/L);
-S_F_low1 =fftshift(S_F_low1);   
+% figure,freqz(Num1b,Den1b);
+% figure,freqz(Num2b,Den2b);
+% figure,freqz(Num3b,Den3b);
+% 
+% figure,freqz(Num1c,Den1c);
+% figure,freqz(Num2c,Den2c);
+% figure,freqz(Num3c,Den3c);
+% 
+% figure,freqz(Num1e,Den1e);
+% figure,freqz(Num2e,Den2e);
+% figure,freqz(Num3e,Den3e);
 
-s_f_low2 = filter(num_low2,den_low2,s);
-S_F_low2 = fft(s_f_low2);
-S_F_low2 = abs(2*S_F_low2/L);
-S_F_low2 =fftshift(S_F_low2);  
+s_f_1b = filter(Num1b,Den1b,s);
+S_F_1b = fft(s_f_1b);
+S_F_1b = abs(2*S_F_1b/L);
+S_F_1b =fftshift(S_F_1b);   
 
-s_f_low3 = filter(num_low3,den_low3,s);
-S_F_low3 = fft(s_f_low3);
-S_F_low3 = abs(2*S_F_low3/L);
-S_F_low3 =fftshift(S_F_low3);  
+s_f_1c = filter(Num1c,Den1c,s);
+S_F_1c = fft(s_f_1c);
+S_F_1c = abs(2*S_F_1c/L);
+S_F_1c =fftshift(S_F_1c);   
+
+s_f_1e = filter(Num1e,Den1e,s);
+S_F_1e = fft(s_f_1e);
+S_F_1e = abs(2*S_F_1e/L);
+S_F_1e =fftshift(S_F_1e);  
+
+
+s_f_2b = filter(Num2b,Den2b,s);
+S_F_2b = fft(s_f_2b);
+S_F_2b = abs(2*S_F_2b/L);
+S_F_2b =fftshift(S_F_2b);  
+
+s_f_2c = filter(Num2c,Den2c,s);
+S_F_2c = fft(s_f_2c);
+S_F_2c = abs(2*S_F_2c/L);
+S_F_2c =fftshift(S_F_2c);   
+
+s_f_2e = filter(Num2e,Den2e,s);
+S_F_2e = fft(s_f_2e);
+S_F_2e = abs(2*S_F_2e/L);
+S_F_2e =fftshift(S_F_2e);  
+
+
+s_f_3b = filter(Num3b,Den3b,s);
+S_F_3b = fft(s_f_3b);
+S_F_3b = abs(2*S_F_3b/L);
+S_F_3b =fftshift(S_F_3b);  
+
+s_f_3c = filter(Num3c,Den3c,s);
+S_F_3c = fft(s_f_3c);
+S_F_3c = abs(2*S_F_3c/L);
+S_F_3c =fftshift(S_F_3c);   
+
+s_f_3e = filter(Num3e,Den3e,s);
+S_F_3e = fft(s_f_3e);
+S_F_3e = abs(2*S_F_3e/L);
+S_F_3e =fftshift(S_F_3e);  
+
 
 %% Gráficos
 figure;
@@ -47,27 +89,73 @@ title('Sinal')
 xlabel('t')
 ylabel('s(t)')
 subplot(2,1,2),plot(freq,S)
-xlim([-1000, 1000])
+axis([-1000 1000 0 1])
 title('Espectro de Amplitude de s(t)')
 xlabel('f (Hz)')
 ylabel('|S(f)|')
 
+% Filtro 1
+
 figure;
-subplot(3,1,1),plot(freq,S_F_low1)
-title('Espectro de Amplitude do sinal Filtrado 1')
-xlim([-1000, 1000])
+subplot(3,1,1),plot(freq,S_F_1b)
+title('Espectro de Amplitude do sinal Filtrado 1 (Butterworth)')
+axis([-1000 1000 0 1])
 xlabel('f (Hz)')
 ylabel('|S(f)|')
 
-subplot(3,1,2),plot(freq,S_F_low2)
-title('Espectro de Amplitude do sinal Filtrado 2')
-xlim([-1000, 1000])
+subplot(3,1,2),plot(freq,S_F_1c)
+title('Espectro de Amplitude do sinal Filtrado 1 (Chebyshev)')
+axis([-1000 1000 0 1])
 xlabel('f (Hz)')
 ylabel('|S(f)|')
 
-subplot(3,1,3),plot(freq,S_F_low3)
-title('Espectro de Amplitude do sinal Filtrado 3')
-xlim([-1000, 1000])
+subplot(3,1,3),plot(freq,S_F_1e)
+title('Espectro de Amplitude do sinal Filtrado 1 (Elíptico)')
+axis([-1000 1000 0 1])
+% set_xticks(-1000:50:1000)
+xlabel('f (Hz)')
+ylabel('|S(f)|')
+
+% Filtro 2
+
+figure;
+subplot(3,1,1),plot(freq,S_F_2b)
+title('Espectro de Amplitude do sinal Filtrado 2 (Butterworth)')
+axis([-1000 1000 0 1])
+xlabel('f (Hz)')
+ylabel('|S(f)|')
+
+subplot(3,1,2),plot(freq,S_F_2c)
+title('Espectro de Amplitude do sinal Filtrado 2 (Chebyshev)')
+axis([-1000 1000 0 1])
+xlabel('f (Hz)')
+ylabel('|S(f)|')
+
+subplot(3,1,3),plot(freq,S_F_2e)
+title('Espectro de Amplitude do sinal Filtrado 2 (Elíptico)')
+axis([-1000 1000 0 1])
+% set_xticks(-1000:50:1000)
+xlabel('f (Hz)')
+ylabel('|S(f)|')
+
+% Filtro 3
+
+figure;
+subplot(3,1,1),plot(freq,S_F_3b)
+title('Espectro de Amplitude do sinal Filtrado 3 (Butterworth)')
+axis([-1000 1000 0 1])
+xlabel('f (Hz)')
+ylabel('|S(f)|')
+
+subplot(3,1,2),plot(freq,S_F_3c)
+title('Espectro de Amplitude do sinal Filtrado 3 (Chebyshev)')
+axis([-1000 1000 0 1])
+xlabel('f (Hz)')
+ylabel('|S(f)|')
+
+subplot(3,1,3),plot(freq,S_F_3e)
+title('Espectro de Amplitude do sinal Filtrado 3 (Elíptico)')
+axis([-1000 1000 0 1])
 % set_xticks(-1000:50:1000)
 xlabel('f (Hz)')
 ylabel('|S(f)|')
